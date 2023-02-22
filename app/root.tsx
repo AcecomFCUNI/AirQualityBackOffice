97ce415fae536f3c34c91a14bb5328a03469323a
AnthonyLzq <sluzquinosa@uni.pe>
2023-02-22 17:18:01 -0500

    fix: page title

--- app/root.tsx
+++ app/root.tsx
@@ -44,7 +44,7 @@ export const links: LinksFunction = () => [
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'Water Quality Back Office',
+  title: 'Air Quality Back Office',
   viewport: 'width=device-width,initial-scale=1',
   'Content-Security-Policy': {
     httpEquiv: 'Content-Security-Policy',
